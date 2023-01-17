216239c79c0508f4ae43bc64912452c04fbfa2f8
priya2201 <priyamurpani99@gmail.com>
2023-01-17 12:40:48 +0530

    practice

--- test.js
+++ test.js
@@ -15,40 +15,42 @@
 
 let x="piyu vinit";
 console.log=x.substr(0,5);
-// console.log=x.substr(0,5);
-// console.log=x.substr(0,5);
-// console.log=x.substr(0,5);
+console.log=x.substr(0,5);
+console.log=x.substr(0,5);
+console.log=x.substr(0,5);
 //console.log(x.replace(/i/,"I"));
 
-// let n=min(4,5);
-// console.log(n);
+let n=min(4,5);
+//console.log(n);
 
-// function min(a,b){
-//     return(a<b)?a:b;
-// }
-
-
-// const output=FizzBuzz(7);
-// console.log(output);
-
-// function FizzBuzz(input){
-//     if( typeof input !== 'number')
-//     return NaN;
-//     if(input%3===0 && input %5===0)
-//     return "FizzBuzz";
-//      if(input %3===0)
-//      return "Fizz";
-//      if(input %5===0)
-//      return "Buzz";
-
-// return input;
-// }
+function min(a,b){
+    return(a<b)?a:b;
+}
 
 
-// shownumbers(20);
-// function shownumbers(limit){
-//     for(let i=0;i<=20;i++){
-//     const message=i%2===0?'EVEN':'ODD';
-//     console.log(i,message);
-//     }
-// }
+const output=FizzBuzz(7);
+//console.log(output);
+
+function FizzBuzz(input){
+    if( typeof input !== 'number')
+    return NaN;
+    if(input%3===0 && input %5===0)
+    return "FizzBuzz";
+     if(input %3===0)
+     return "Fizz";
+     if(input %5===0)
+     return "Buzz";
+
+return input;
+}
+
+
+shownumbers(20);
+function shownumbers(limit){
+    for(let i=0;i<=20;i++){
+    const message=i%2===0?'EVEN':'ODD';
+   // console.log(i,message);
+    }
+}
+
+console.log('hi');
